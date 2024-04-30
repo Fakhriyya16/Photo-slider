3bdddc1e5aa08cfc538087fc6a1c9bbce520336a
Taghizada Fakhriyya <fakhriyyant@code.local>
2024-04-30 12:52:20 +0400

    initial

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let rightArrow = document.querySelector(".right-arrow");
+let leftArrow = document.querySelector(".left-arrow");
+
+let images = document.querySelectorAll(".images");
+
+rightArrow.addEventListener("click",function(){
+    
+})
