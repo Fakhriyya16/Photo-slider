f182025edf79e8177f304ed2cde7d2a307fcd4f3
Faha Tagizade <Fakhriyya16>
2024-04-30 15:15:10 +0400

    slider finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,32 @@
 let rightArrow = document.querySelector(".right-arrow");
 let leftArrow = document.querySelector(".left-arrow");
 
-let images = document.querySelectorAll(".images");
+let images = document.querySelectorAll(".images img");
 
-rightArrow.addEventListener("click",function(){
-    
-})
+rightArrow.addEventListener("click", function () {
+    let currentImg = document.querySelector(".active-image");
+    let currentImgId = parseInt(currentImg.getAttribute("data-id"));
+
+    let nextImgId = currentImgId + 1;
+    if (nextImgId > images.length) {
+        nextImgId = 1;
+    }
+
+    currentImg.classList.remove("active-image");
+    let nextImg = Array.from(document.querySelectorAll(".images img")).find(m => m.getAttribute("data-id") == nextImgId);
+    nextImg.classList.add("active-image");
+});
+
+leftArrow.addEventListener("click", function () {
+    let currentImg = document.querySelector(".active-image");
+    let currentImgId = parseInt(currentImg.getAttribute("data-id"));
+
+    let previousImgId = currentImgId - 1;
+    if (previousImgId < 1) {
+        previousImgId = 5;
+    }
+
+    currentImg.classList.remove("active-image");
+    let previousImg = Array.from(document.querySelectorAll(".images img")).find(m => m.getAttribute("data-id") == previousImgId);
+    previousImg.classList.add("active-image");
+});
